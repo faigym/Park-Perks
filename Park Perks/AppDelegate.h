2fb4301e3f3da4c124783156440a5721b6b87ce1
SiEOC <skyler@whitecapinstitute.com>
2015-05-19 16:14:22 -0600

    first commit
    Created project in git folder

---
+++ Park Perks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Park Perks
+//
+//  Created by Skyler Clark on 5/19/15.
+//  Copyright (c) 2015 Skyler Clark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Park Perks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Park Perks
+//
+//  Created by Skyler Clark on 5/19/15.
+//  Copyright (c) 2015 Skyler Clark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
