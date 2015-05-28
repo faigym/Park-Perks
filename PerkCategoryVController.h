15cae2165a0b2980f4ce037c83109bcd319ab401
Skyler Clark <Skyler@whitecapinstitute.com>
2015-05-27 21:53:22 -0600

    added PerkCategoryClass
    perkClass

---
+++ PerkCategoryVController.h
@@ -0,0 +1,13 @@
+//
+//  PerkCategoryVController.h
+//  Park Perks
+//
+//  Created by Skyler Clark on 5/27/15.
+//  Copyright (c) 2015 Skyler Clark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PerkCategoryVController : UIViewController
+
+@end
