df7032146b39d8aa30d11eae008a743b7d74f2fd
Skyler Clark <Skyler@whitecapinstitute.com>
2015-05-27 22:08:29 -0600

    added ParkDetailsTBVC
    Still need to Connect.  New Name For "DetailView"\
    
    Is  TableViewController
    
    Will Adjust Class: As last minute Designs may occur
    
    UPDATED NOTES PLEASE CONFIRM or just conform ;)

---
+++ ParkDetailsTBVC.h
@@ -0,0 +1,13 @@
+//
+//  ParkDetailsTBVC.h
+//  Park Perks
+//
+//  Created by Skyler Clark on 5/27/15.
+//  Copyright (c) 2015 Skyler Clark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ParkDetailsTBVC : UITableViewController
+
+@end
