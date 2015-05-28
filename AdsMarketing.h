b27f58a160da75f01f0e68ba056f0baf43d0bfe9
Skyler Clark <Skyler@whitecapinstitute.com>
2015-05-27 21:00:41 -0600

    Added Ads Marketing Class
    A bit over the top... Confirm with others (youGuys)
    Do we want this? Best Practice

---
+++ AdsMarketing.h
@@ -0,0 +1,13 @@
+//
+//  AdsMarketing.h
+//  Park Perks
+//
+//  Created by Skyler Clark on 5/27/15.
+//  Copyright (c) 2015 Skyler Clark. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AdsMarketing : NSObject
+
+@end
---
+++ AdsMarketing.m
@@ -0,0 +1,13 @@
+//
+//  AdsMarketing.m
+//  Park Perks
+//
+//  Created by Skyler Clark on 5/27/15.
+//  Copyright (c) 2015 Skyler Clark. All rights reserved.
+//
+
+#import "AdsMarketing.h"
+
+@implementation AdsMarketing
+
+@end
